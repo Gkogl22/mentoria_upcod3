ab3c79d4b1de371b16ab3676a8b3f0935e1a065a
ana <gabivestudos@gmail.com>
2024-03-20 22:32:40 -0300

    desafio sendo feito

---
+++ logica_de_programacao/variaveis/texte.js
@@ -0,0 +1,14 @@
+/*var nome = "gabys"
+console.log(nome)
+let idade = 21
+console.log(idade)*/
+var cor = "rosa"
+if(cor == "rosa")
+{var nome = "gabys"
+
+	let idade = 21
+console.log(idade)
+
+	console.log("é rosa")}
+console.log(nome)
+//console.log(idade)
