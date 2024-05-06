72b8c433178f0fd3f5eb3d7b21f71ccc8ff46d1d
ana <gabivestudos@gmail.com>
2024-05-06 20:36:44 -0300

    desafio 4 efetuado

---
+++ logica_de_programacao/condicionais/desafio_04.js
@@ -0,0 +1,6 @@
+var quadradoA = 35*18
+var quadradoB = 9*12
+
+if(quadradoA > quadradoB){console.log( "Quadrado A é maior que B")}
+ else if(quadradoB > quadradoA){console.log("Quadrado B é maior que A")}
+ else{console.log("os dois são iguais")}
