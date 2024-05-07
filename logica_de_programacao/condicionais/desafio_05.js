8f123564eebd00f59eb9b1443d49fb0780bd1f29
ana <gabivestudos@gmail.com>
2024-05-06 21:16:30 -0300

    desafio 5 comcluido

---
+++ logica_de_programacao/condicionais/desafio_05.js
@@ -0,0 +1,28 @@
+var mes = 8
+
+if (mes == 1){console.log("janeiro");
+} else if( mes == 2){
+    console.log ("fevereiro");
+} else if (mes == 3) {
+     console.log("Março");
+    } else if( mes == 4){
+        console.log ("Abril");
+    } else if( mes == 5){
+        console.log ("Maio");
+    } else if( mes == 6){
+        console.log ("Junho");
+    } else if( mes == 7){
+        console.log ("Julho");
+    } else if( mes == 8){
+        console.log ("Agosto");
+    } else if( mes == 9){
+        console.log ("Setembro");
+    } else if( mes == 10){
+        console.log ("Outubro");
+    } else if( mes == 11){
+        console.log ("Novembro");
+    } else if( mes == 12){
+        console.log ("Desembro");
+    } else{console.log ("numero incorreto por favor digite um numero de 1 á 12")}
+
+    // uma outra forma que pensei em usar era o switch | case | break
